446f01ed0406cbb054a9ee0f8fa30f517eee3b61
masumkawser <kawser151722@gmail.com>
2022-10-26 11:20:48 +0600

    server side okay

--- index.js
+++ index.js
@@ -2,10 +2,13 @@
  const express =require('express')
  const app =express();
  const port=process.env.PORT ||5000;
+ const catagories =require('./data/data.json');
  app.get('/',(req,res) =>{
     res.send('bINARTY CATAGORY RUNNING');
  })
-
+ app.get('/course-catagories',(req,res) =>{
+    res.send(catagories);
+ })
  app.listen(port,()=>{
     console.log('binary academy port running',port);
  })
